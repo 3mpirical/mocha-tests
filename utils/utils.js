ef74ecd815c6e3fde6f5e9a60637cdd0acccdb24
3mpirical <3mpirical10510@gmail.com>
2018-08-10 23:12:46 -0600

    ty functions and more tests added
    *assertions with chai added
    *asynchronous functions added
    *asynchronous tests using done() added
    *various chai methods explored

--- utils/utils.js
+++ utils/utils.js
@@ -6,12 +6,38 @@ const utils = {
     return a + b;
   },
 
+  asyncAdd: (a, b, callback) => {
+    setTimeout(() => {
+      callback(a + b);
+    }, 300);
+  },
+
   square: (x) => {
     return x * x;
+  },
+
+  asyncSquare: (x) => {
+    return new Promise( (resolve, reject) => {
+      setTimeout(() => {
+        resolve(x * x);
+      }, 300);
+    });
+  },
+
+  setName: (userObj, fullName) => {
+    if(typeof fullName !== "string") {
+      throw new Error("fullName argument is not a string");
+    } else{
+      let fullNameArr = fullName.split(" ");
+
+      userObj.firstName = fullNameArr[0];
+      userObj.lastName = fullNameArr[1];
+
+      return userObj;
+    }
   }
-
-
 };
 
+// console.log( JSON.stringify(utils.setName({}, "Jerry Mandering"), null, 2) );
 
 module.exports = utils;
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,18 +1,62 @@
-const utils = require("./utils.js");
+const chai    = require("chai"),
+      expect  = chai.expect,
+
+      utils = require("./utils.js");
+
 
 
 it("should add two numbers", () => {
   const result = utils.add(42, 11);
 
-  if(result !== 53) {
-    throw new Error(`Expected 53, but got ${result}`);
-  }
+  expect(result)
+    .to.equal(53);
 });
+
+
+it("should add two numbers asynchronously", (done) => {
+  utils.asyncAdd(4, 6, (result) => {
+    expect(result)
+      .to.be.a("number")
+      .to.equal(10);
+    done();
+  });
+});
+
 
 it("should square a number", () => {
   const result = utils.square(5);
 
-  if(result !== 25) {
-    throw new Error(`Expected 25, but got ${result}`);
-  }
+  expect(result)
+    .to.equal(25);
+});
+
+
+it("should square a number asynchronously", (done) => {
+  utils.asyncSquare(6)
+    .then((result) => {
+      expect(result)
+        .to.be.a("number")
+        .to.equal(36);
+      done();
+    });
+});
+
+
+it("should set firstName and lastName properties", () => {
+  const result = utils.setName({
+    age: 34,
+    location: "Chicago"
+  }, "Jerry Mandering");
+
+  expect(result)
+    .to.be.an("object")
+    .to.include({firstName: "Jerry"})
+    .to.include({lastName: "Mandering"})
+    .to.deep.equal({
+      age: 34,
+      location: "Chicago",
+      firstName: "Jerry",
+      lastName: "Mandering"
+    });
+
 });
